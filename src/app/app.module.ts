f5daace4f247488714116c9b3f9eb09eefad2d0d
AdrinocDev <adriandevop@outlook.es>
2020-01-14 16:53:02 +0100

    Agrego Home Component y lo intento vincular al router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
